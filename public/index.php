24afad865eadb230a8dfc0fc57673aaeee89e59e
AlexanderWiman <alexander.wiman@cmeducations.se>
2022-06-09 22:43:43 +0200

    bootstrap link

--- public/index.php
+++ public/index.php
@@ -1,6 +1,48 @@
 <?php include('layout/header.php') ?>
 
 
-	<!-- <img src="img/banner.jpg" alt="" class="banner"> -->
+<div class="row">
+  <div class="col-lg-4 col-md-12 mb-4 mb-lg-0">
+    <img
+      src="img/products/Adidas_Ivy Park.jpeg"
+      class="w-100 shadow-1-strong rounded mb-4"
+      alt="Boat on Calm Water"
+    />
+
+    <img
+      src="img/products/Nike_Air-Force_01.jpeg"
+      class="w-100 shadow-1-strong rounded mb-4"
+      alt="Wintry Mountain Landscape"
+    />
+  </div>
+
+  <div class="col-lg-4 mb-4 mb-lg-0">
+    <img
+      src="img/products/Nike_Air-Force_02.jpeg"
+      class="w-100 shadow-1-strong rounded mb-4"
+      alt="Mountains in the Clouds"
+    />
+
+    <img
+      src="img/products/Nike_Air-Force_03.jpeg"
+      class="w-100 shadow-1-strong rounded mb-4"
+      alt="Boat on Calm Water"
+    />
+  </div>
+
+  <div class="col-lg-4 mb-4 mb-lg-0">
+    <img
+      src="img/products/Nike_Air-Force_04.jpeg"
+      class="w-100 shadow-1-strong rounded mb-4"
+      alt="Waves at Sea"
+    />
+
+    <img
+      src="img/products/Nike_Air-Force_05.jpeg"
+      class="w-100 shadow-1-strong rounded mb-4"
+      alt="Yosemite National Park"
+    />
+  </div>
+</div>
 </body>
 </html>
