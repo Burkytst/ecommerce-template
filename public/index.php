ab8836192053d667a2aaef7b77bb77d65d99c49c
AlexanderWiman <alexander.wiman@cmeducations.se>
2022-06-10 15:33:01 +0200

    Small changes

--- public/index.php
+++ public/index.php
@@ -10,7 +10,7 @@ $products = $stmt->fetchAll();
 
 		<?php foreach($products as $product) { ?>
         <div class="col-lg-4 col-md-12 mb-4 mb-lg-0">
-            <img src="img/products/<?=$product['img_url']?>" class="w-100 shadow-1-strong rounded mb-4">
+            <img src="img/products/<?=$product['img_url']?>" class="productThumb w-100 shadow-1-strong rounded mb-4">
 			
 		</div>
             
