6d6ab05651bc9e7cc31d3b34efd098324e16ab80
AlexanderWiman <alexander.wiman@cmeducations.se>
2022-06-10 15:21:48 +0200

    db connected

--- public/index.php
+++ public/index.php
@@ -1,21 +1,21 @@
 <?php include('layout/header.php');
 
-$sql = "
-SELECT * FROM products
-";
-$stmt = $dbconnect->prepare($sql);
-$stmt->execute();
-$products = $stmt->fetch();
+$sql = "SELECT * FROM products";
+$stmt = $dbconnect->query($sql);
+$products = $stmt->fetchAll();
 ?>
 
 
 <div class="row">
 
-
+		<?php foreach($products as $product) { ?>
         <div class="col-lg-4 col-md-12 mb-4 mb-lg-0">
-            <img src="img/products/<?=$products['img_url']?>" class="w-100 shadow-1-strong rounded mb-4">
-        </div>
- 
+            <img src="img/products/<?=$product['img_url']?>" class="w-100 shadow-1-strong rounded mb-4">
+			
+		</div>
+            
+
+    <?php } ?>
 </div>
 </body>
 </html>
