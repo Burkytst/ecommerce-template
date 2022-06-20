8d798d96826b3c90eb2a34018f6844f46450d2fb
Miggorie <info@elinselin.com>
2022-06-20 11:54:47 +0200

    test2

--- public/admin/layout/admin-user.php
+++ public/admin/layout/admin-user.php
@@ -1,13 +1,13 @@
 <?php
-	require('../src/config.php');
+	// require('../src/config.php');
     require('../src/dbconnect.php');
 
-// If the user is not logged in redirect to the login page...
-if (!isset($_SESSION['loggedin'])) {
-	header('Location: index.html');
-	exit;
+// // // If the user is not logged in redirect to the login page...
+// // if (!isset($_SESSION['loggedin'])) {
+// // 	header('Location: index.html');
+// // 	exit;
 
-}
+// }
 ?>
 
 <!DOCTYPE html>
