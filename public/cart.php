476f628ad59bc02a2615a1b69dfdb3b8c47d36d7
AlexanderWiman <alexander.wiman@cmeducations.se>
2022-06-28 07:58:56 +0200

    Cart updates

--- public/cart.php
+++ public/cart.php
@@ -2,6 +2,13 @@
     require('../src/config.php');
     require('../src/dbconnect.php');
 
+
+    $userID =  $_SESSION['id'];
+
+    $sql = "SELECT * FROM users WHERE id=$userID";
+    $stmt = $dbconnect->query($sql);
+    $user = $stmt->fetch();
+
 // If the user clicked the add to cart button on the product page we can check for the form data
 if (isset($_POST['product_id'], $_POST['quantity']) && is_numeric($_POST['product_id']) && is_numeric($_POST['quantity'])) {
     
@@ -94,11 +101,7 @@ if ($products_in_cart) {
                     <a href="index.php"><div class="d-flex flex-row align-items-center"><i class="fa fa-long-arrow-left"></i><span class="ml-2">Continue Shopping</span></div></a>
                     <hr>
                     <h6 class="mb-0">Shopping cart</h6>
-                    <div class="d-flex justify-content-between"><span>You have <?=$subquantity?> items in your cart</span>
-                        <div class="d-flex flex-row align-items-center"><span class="text-black-50">Sort by:</span>
-                            <div class="price ml-2"><span class="mr-1">price</span><i class="fa fa-angle-down"></i></div>
-                        </div>
-                    </div>
+
         <?php foreach ($products as $product): ?>
                     <div class="d-flex justify-content-between align-items-center mt-3 p-2 items rounded">
                         <div class="d-flex flex-row"><img class="rounded" src="img/products/<?=$product['img_url']?>" width="40">
@@ -116,20 +119,13 @@ if ($products_in_cart) {
             <div class="col-md-4">
                 <div class="payment-info">
                 
-                    <div class="d-flex justify-content-between align-items-center"><span>Card details</span><img class="rounded" src="https://i.imgur.com/WU501C8.jpg" width="30"></div><span class="type d-block mt-3 mb-1">Card type</span><label class="radio"> <input type="radio" name="card" value="payment" checked> <span><img width="30" src="https://img.icons8.com/color/48/000000/mastercard.png"/></span> </label>
+                    <div class="d-flex justify-content-between align-items-center"><span>Shipping details</span></div>
 
-                    <label class="radio"> <input type="radio" name="card" value="payment"> <span><img width="30" src="https://img.icons8.com/officel/48/000000/visa.png"/></span> </label>
+                    <div><label class="credit-card-label"><?=$user['first_name']?><?=$user['last_name']?></label></div>
+                    <div><label class="credit-card-label"><?=$user['street']?></label></div>
+                    <div><label class="credit-card-label"><?=$user['postal_code']?></label></div>
+                    <div><label class="credit-card-label"><?=$user['city']?></label></div>
 
-                    <label class="radio"> <input type="radio" name="card" value="payment"> <span><img width="30" src="https://img.icons8.com/ultraviolet/48/000000/amex.png"/></span> </label>
-
-
-                    <label class="radio"> <input type="radio" name="card" value="payment"> <span><img width="30" src="https://img.icons8.com/officel/48/000000/paypal.png"/></span> </label>
-                    <div><label class="credit-card-label">Name on card</label><input type="text" class="form-control credit-inputs" placeholder="Name"></div>
-                    <div><label class="credit-card-label">Card number</label><input type="text" class="form-control credit-inputs" placeholder="0000 0000 0000 0000"></div>
-                    <div class="row">
-                        <div class="col-md-6"><label class="credit-card-label">Date</label><input type="text" class="form-control credit-inputs" placeholder="12/24"></div>
-                        <div class="col-md-6"><label class="credit-card-label">CVV</label><input type="text" class="form-control credit-inputs" placeholder="342"></div>
-                    </div>
                     <hr class="line">
                     <div class="d-flex justify-content-between information"><span>Subtotal</span><span>$<?=$subtotal?></span></div>
                     <div class="d-flex justify-content-between information"><span>Shipping</span><span>$20.00</span></div>
--- src/registerValidation.php
+++ src/registerValidation.php
@@ -18,7 +18,7 @@ $errors = array();
 
 $DATABASE_HOST = 'localhost';
 $DATABASE_USER = 'root';
-$DATABASE_PASS = '';
+$DATABASE_PASS = 'root';
 $DATABASE_NAME = 'webshop';
 // Try and connect using the info above.
 $db = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
